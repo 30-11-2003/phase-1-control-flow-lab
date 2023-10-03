6cdb8d2cb4671f357634051df4d6b094244d668c
ShalomOrangi <145541724+30-11-2003@users.noreply.github.com>
2023-10-03 19:18:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if (num <= 400) {
+    cosole.log ("This is on me!")
+  } else if (num > 2000) {
+    console.log("I will gladly take your 30 bucks.")
+  } else if (num > 2500) {
+    console.log("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  if( cityName==="NYC")? "Ok sounds good." :"No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case ="generous":
+      console.log("Thank you so much.");
+      break;
+    case = "not generous":
+      cosole.log("Thank you.");
+      break;
+  }else { 
+    console.log ("bye")
+  }             
 }
